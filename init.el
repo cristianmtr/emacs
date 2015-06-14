becc525dedf9335379e34cb24d15a3860acd95ca
cristian mtr <cristi.mtr@gmail.com>
2015-06-14 15:41:05 +0200

    theme from gui

--- init.el
+++ init.el
@@ -216,4 +216,16 @@
 (define-key global-map (kbd "C-q") 'ace-jump-mode)
 
 
-
+;; finally custom set variables from the GUI menus
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tsdh-light))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
