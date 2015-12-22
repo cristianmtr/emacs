51a9772541939d7f47353a98ba1a148dfbf565c0
Administrator <administrator@notdefined.com>
2015-12-22 10:28:50 +0100

    org mode exports latex src sections in minted mode

--- init.el
+++ init.el
@@ -303,3 +303,11 @@
 
 (setq org-src-fontify-natively t)
 
+(require 'ox-latex)
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+(setq org-latex-listings 'minted)
+
+(setq org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
