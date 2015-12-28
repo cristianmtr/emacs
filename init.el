9e841a241b3403e2cf832f44a68366d6d9e8a47d
cristimtr <cristi.mtr@gmail.com>
2015-12-28 10:31:42 +0100

    tabs and indentation

--- init.el
+++ init.el
@@ -255,6 +255,36 @@
 (global-set-key (kbd "M--") 'elpy-nav-move-iblock-left)
 (global-set-key (kbd "M-=") 'elpy-nav-move-iblock-right)
 
+;; tabs, oh tabs
+(defun how-many-region (begin end regexp &optional interactive)
+  "Print number of non-trivial matches for REGEXP in region.                    
+Non-interactive arguments are Begin End Regexp"
+  (interactive "r\nsHow many matches for (regexp): \np")
+  (let ((count 0) opoint)
+    (save-excursion
+      (setq end (or end (point-max)))
+      (goto-char (or begin (point)))
+      (while (and (< (setq opoint (point)) end)
+                  (re-search-forward regexp end t))
+        (if (= opoint (point))
+            (forward-char 1)
+          (setq count (1+ count))))
+      (if interactive (message "%d occurrences" count))
+      count)))
+
+(defun infer-indentation-style ()
+  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
+  ;; neither, we use the current indent-tabs-mode                               
+  (let ((space-count (how-many-region (point-min) (point-max) "^  "))
+        (tab-count (how-many-region (point-min) (point-max) "^\t")))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4)
+(setq tab-stop-list (number-sequence 4 120 4))
+(infer-indentation-style)
+
 ;; keybindings for scrolling
 (global-set-key (kbd "C-M-n") 'scroll-up-command)
 (global-set-key (kbd "C-M-p") 'scroll-down-command)
